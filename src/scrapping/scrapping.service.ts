58bdd4ed7250990416148dd60700ac4b02734ec0
BoukorrasLuc <l.boukorras@gmail.com>
2023-11-13 20:52:13 +0100

    feat(scrapping): add try catch

--- src/scrapping/scrapping.service.ts
+++ src/scrapping/scrapping.service.ts
@@ -65,140 +65,178 @@ export class ScrappingService {
         // Create the URL and launch the browser
         const url: string = createUrl(SEARCH_CONFIG);
         const browser = await puppeteer.launch({ headless: 'new' });
-        const page = await browser.newPage();
-        await page.goto(url);
 
-        // Scrape the data from the eBay page
-        const data = await page.evaluate(
-          (searchTerm: string, title: string) => {
-            const tds = Array.from(document.querySelectorAll('.s-item'));
+        try {
+          const page = await browser.newPage();
+          await page.goto(url);
 
-            return tds
-              .map((td) => {
-                try {
-                  const price: string | null =
-                    (td.querySelector('span.s-item__price') as HTMLElement)
-                      ?.innerText || null;
-                  const name: string | null =
-                    (
-                      td.querySelector('div.s-item__title') as HTMLElement
-                    )?.innerText.toLowerCase() || null;
-                  const date: string | null =
-                    (
-                      td.querySelector(
-                        'div.s-item__title--tag span.POSITIVE',
-                      ) as HTMLElement
-                    )?.innerText.replace('Vendu le', '') || null;
-                  const typeSold: string | null =
-                    (
-                      td.querySelector(
-                        'span.s-item__purchaseOptionsWithIcon',
-                      ) as HTMLElement
-                    )?.innerText || null;
-                  const country: string | null =
-                    (
-                      td.querySelector(
-                        'span.s-item__location.s-item__itemLocation',
-                      ) as HTMLElement
-                    )?.innerText || null;
-
-                  return {
-                    price,
-                    name,
-                    date,
-                    typeSold,
-                    country,
-                  };
-                } catch (error) {
-                  this.logger.error(
-                    `[Scrapping Service] Error found in map: ${error}`,
-                  );
-                  return null;
-                }
-              })
-              .filter(
-                (item) =>
-                  item != null &&
-                  item.name.includes(searchTerm) &&
-                  item.name.includes(title.toLowerCase()),
-              );
-          },
-          SEARCH_CONFIG.searchTerm,
-          SEARCH_CONFIG.title,
-        );
-
-        // If data was found, transform it
-        if (data.length > 0) {
-          const item = data[0];
-          const matchTitleZone = item.name.match(regexZone);
-
-          const matchTitleCompleteness = item.name.match(regexCompleteness);
-
-          const matchCondition = item.name.match(regexCondition);
-
-          const newTitle = item.name
-            .replace(regexDeleteElementOfTitle, '')
-            .trim();
-
-          // Remove console name from title
-          const regexConsoleName = new RegExp(game.consoleName, 'gi');
-          const finalTitle = newTitle.replace(regexConsoleName, '').trim();
-
-          const transformedItem = {
-            title: capitalizeFirstLetterOfEachWord(getSpecificText(finalTitle)),
-            priceSold: item.price ? item.price.split(' ')[0] : null,
-            dateSold: item.date ? item.date : null,
-            condition: matchCondition
-              ? capitalizeFirstLetterOfEachWord(replaceEtat(matchCondition[0]))
-              : null,
-            completeness: matchTitleCompleteness
-              ? capitalizeFirstLetterOfEachWord(matchTitleCompleteness[0])
-              : null,
-            zone: matchTitleZone
-              ? capitalizeFirstLetterOfEachWord(matchTitleZone[0])
-              : null,
-            console: game.consoleName,
-          };
-          
-          
-          const { condition, ...otherProps } = transformedItem;
-          
-          if (Object.values(otherProps).every((value) => value !== null)) {
-            console.log(
-              '🚀 ~ file: scrapping.service.ts:167 ~ ScrappingService ~ scrapeEbay ~ transformedItem:',
-              transformedItem,
+          // Check if "Aucun résultat correspondant n'a été trouvé" was found, if so, skip this iteration
+          const noResultsFound = await page.evaluate(() => {
+            return (
+              document.querySelector('.srp-save-null-search__heading')
+                ?.textContent === "Aucun résultat correspondant n'a été trouvé"
             );
+          });
+          if (noResultsFound) {
+            console.log(
+              '🚀 ~ file: scrapping.service.ts:76 ~ ScrappingService ~ scrapeEbay ~ noResultsFound:',
+              noResultsFound,
+            );
+            continue;
+          }
 
-            const gameToUpdate = await this.gameService.getById(game.id);
+          // Scrape the data from the eBay page
+          const data = await page.evaluate(
+            (searchTerm: string, title: string) => {
+              const tds = Array.from(document.querySelectorAll('.s-item'));
 
-            if (
-              gameToUpdate.productName.toUpperCase() === transformedItem.title.toUpperCase() &&
-              gameToUpdate.consoleName.toUpperCase() === transformedItem.console.toUpperCase() &&
-              gameToUpdate.zone.toUpperCase() ===
-                transformedItem.zone.toUpperCase()
-            ) {
-              await this.gameService.updateById(gameToUpdate.id, {
-                cibPrice: parseFloat(
-                  transformedItem.priceSold.replace(',', '.'),
-                ),
-                ebayDate: transformedItem.dateSold.substring(1),
-              } as UpdateGameDto);
-              await this.logger.log(
-                `[Scrapping Service] This game is update: ${JSON.stringify(
-                  gameToUpdate,
-                )}`,
+              return tds
+                .map((td) => {
+                  try {
+                    const price: string | null =
+                      (td.querySelector('span.s-item__price') as HTMLElement)
+                        ?.innerText || null;
+                    const name: string | null =
+                      (
+                        td.querySelector('div.s-item__title') as HTMLElement
+                      )?.innerText.toLowerCase() || null;
+                    const date: string | null =
+                      (
+                        td.querySelector(
+                          'div.s-item__title--tag span.POSITIVE',
+                        ) as HTMLElement
+                      )?.innerText.replace('Vendu le', '') || null;
+                    const typeSold: string | null =
+                      (
+                        td.querySelector(
+                          'span.s-item__purchaseOptionsWithIcon',
+                        ) as HTMLElement
+                      )?.innerText || null;
+                    const country: string | null =
+                      (
+                        td.querySelector(
+                          'span.s-item__location.s-item__itemLocation',
+                        ) as HTMLElement
+                      )?.innerText || null;
+
+                    return {
+                      price,
+                      name,
+                      date,
+                      typeSold,
+                      country,
+                    };
+                  } catch (error) {
+                    this.logger.error(
+                      `[Scrapping Service] Error found in map: ${error}`,
+                    );
+                    return null;
+                  }
+                })
+                .filter(
+                  (item) =>
+                    item != null &&
+                    item.name.includes(searchTerm) &&
+                    item.name.includes(title.toLowerCase()),
+                );
+            },
+            SEARCH_CONFIG.searchTerm,
+            SEARCH_CONFIG.title,
+          );
+
+          // If data was found, transform it
+          if (data.length > 0) {
+            const item = data[0];
+            const matchTitleZone = item.name.match(regexZone);
+
+            const matchTitleCompleteness = item.name.match(regexCompleteness);
+
+            const matchCondition = item.name.match(regexCondition);
+
+            const newTitle = item.name
+              .replace(regexDeleteElementOfTitle, '')
+              .trim();
+
+            // Remove console name from title
+            const regexConsoleName = new RegExp(game.consoleName, 'gi');
+            const finalTitle = newTitle.replace(regexConsoleName, '').trim();
+
+            const transformedItem = {
+              title: capitalizeFirstLetterOfEachWord(
+                getSpecificText(finalTitle),
+              ),
+              priceSold: item.price ? item.price.split(' ')[0] : null,
+              dateSold: item.date ? item.date : null,
+              condition: matchCondition
+                ? capitalizeFirstLetterOfEachWord(
+                    replaceEtat(matchCondition[0]),
+                  )
+                : null,
+              completeness: matchTitleCompleteness
+                ? capitalizeFirstLetterOfEachWord(matchTitleCompleteness[0])
+                : null,
+              zone: matchTitleZone
+                ? capitalizeFirstLetterOfEachWord(matchTitleZone[0])
+                : null,
+              console: game.consoleName,
+            };
+
+            const { condition, ...otherProps } = transformedItem;
+
+            if (Object.values(otherProps).every((value) => value !== null)) {
+              console.log(
+                '🚀 ~ file: scrapping.service.ts:167 ~ ScrappingService ~ scrapeEbay ~ transformedItem:',
+                transformedItem,
               );
+
+              const gameToUpdate = await this.gameService.getById(game.id);
+
+              if (
+                gameToUpdate.productName.toUpperCase() ===
+                  transformedItem.title.toUpperCase() &&
+                gameToUpdate.consoleName.toUpperCase() ===
+                  transformedItem.console.toUpperCase() &&
+                gameToUpdate.zone.toUpperCase() ===
+                  transformedItem.zone.toUpperCase()
+              ) {
+                await this.gameService.updateById(gameToUpdate.id, {
+                  cibPrice: parseFloat(
+                    transformedItem.priceSold.replace(',', '.'),
+                  ),
+                  ebayDate: transformedItem.dateSold.substring(1),
+                } as UpdateGameDto);
+                await this.logger.log(
+                  `[Scrapping Service] This game is update: ${JSON.stringify(
+                    gameToUpdate,
+                  )}`,
+                );
+              }
             }
           }
-        }
 
-        // Close the browser and wait for 1 minute before the next iteration
-        await browser.close();
-        await new Promise((resolve) => setTimeout(resolve, 10000));
+          // Close the browser and wait for 1 minute before the next iteration
+          await browser.close();
+          await new Promise((resolve) => setTimeout(resolve, 10000));
+        } catch (error) {
+          console.log(
+            '🚀 ~ file: scrapping.service.ts:212 ~ ScrappingService ~ scrapeEbay ~ error:',
+            error,
+          );
+        } finally {
+          if (browser) {
+            await browser.close();
+          }
+        }
       }
-      
     }
   }
 }
 
 // Notice Seul : https://www.ebay.fr/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=+notice+seul+gamecube&_sacat=0&LH_TitleDesc=0&_odkw=Metroid+Prime+notice+seul+gamecube&_osacat=0&LH_Complete=1&LH_Sold=1
+
+// Bug avec cette url. https://www.ebay.fr/sch/i.html?_from=R40&_trksid=p2334524.m570.l1313&_nkw=Colin+Mcrae+Rally+gameboy+color+complet&_sacat=0&LH_TitleDesc=0&rt=nc&_odkw=Colin+Mcrae+Rally+game+boy+complet&_osacat=0&LH_Complete=1&LH_Sold=1
+
+// Aucun résultat correspondant n'a été trouvé du coup c'est prends la premiere annonce qui ne correspondant du coup. Du coup la il a enregistrer la mauvaise annonce. le nom du jeux correspondant mais pas la console.
+// Résultats correspondant à moins de mots
+
+// Débug Ok.
